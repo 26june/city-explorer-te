7c1055bc0afe4d1b8854b94237aa84ff16442b1a
26june <gj.abordo1@gmail.com>
2023-10-30 13:27:15 +0000

    show name, lat and long

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    const res = await axios.get(API);
+
+    setLocation(res.data[0]);
+  }
+
   return (
     <>
-      <p>Hello</p>
+      <h1>APIs</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Location" />
+        <button>Get Location</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+      <p>{location.lat}</p>
+      <p>{location.lon}</p>
     </>
   );
 }
